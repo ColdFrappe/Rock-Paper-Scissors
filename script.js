2956fc2c00aa32f957a8fb83386fd22340c37789
ColdFrappeGit <kocbaran06@hotmail.com>
2022-10-26 11:58:03 +0300

    Add score functionality

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 function getComputerChoice() {
-    const choicesArray = ["Rock", "Paper", "Scissors"];
+    const choicesArray = ["rock", "paper", "scissors"];
     let choiceComputer = choicesArray[Math.floor(Math.random() * 3)];
     return choiceComputer;
 }
-console.log(getComputerChoice());
 
-function playGame(playerSelection, computerSelection) {
-    if playerSelection === "Rock" && computerSelection === "Rock" {
-        console.log("Draw!") /*continue from here make the game functions*/
+const computerSelection = getComputerChoice()
+
+let playerSelection
+
+function playRound(playerSelection = prompt('Rock, paper, scissors?').toLowerCase(), computerSelection = getComputerChoice()) {
+    if (playerSelection === computerSelection) {
+        return 'Draw'
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore = playerScore + 1
+        return 'Rock beats scissors, player wins.'
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore = computerScore + 1
+        return 'Paper beats rock, computer wins.'
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore = computerScore + 1
+        return 'Rock beats scissors, computer wins.'
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore = playerScore + 1
+        return 'Scissors beats paper, player wins.'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore = playerScore + 1
+        return 'Paper beats rock, player wins.'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore = computerScore + 1
+        return 'Scissors beats paper, computer wins.'
+    }
+    else {
+        return 'Error: Please type your choice correctly.'
     }
 }
 
-const computerSelection = getComputerChoice()
-const playerSelection = prompt("Rock, paper, scissors?")
+let computerScore = 0
+
+let playerScore = 0
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound())
+        console.log('Computer Score: ' + computerScore)
+        console.log('Player Score: ' + playerScore)
+        console.log(i)
+    }
+}
